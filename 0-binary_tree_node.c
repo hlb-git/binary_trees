99c055c5a8906973519f2b71ca84feb0b3e0d6b0
Love <mzeelovegeneral@gmail.com>
2024-01-03 20:51:17 +0100

    remove parent constraint

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,13 +15,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (!new_node)
 		return (NULL);
 
-	if (parent)
-		new_node->parent = parent;
-	if (value)
-		new_node->n = value;
-
+	new_node->parent = parent;
+	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
 	return (new_node);
 }
-
